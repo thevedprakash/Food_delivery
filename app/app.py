d6374d340c8886357e757c434da322ae378e280c
Ved <thevedprakash.in@gmail.com>
2023-10-16 10:39:30 +0800

    Fixing Website bugs.

--- app/app.py
+++ app/app.py
@@ -5,9 +5,7 @@ import pickle
 import pandas as pd
 from flask_cors import CORS
 from flask import Flask, json, request, jsonify , render_template
-from werkzeug.utils import secure_filename
 from predict import preprocess_and_predict
-from variables import airlines, sources, destinations, routes
 
 
 # This will enable CORS for all routes
@@ -21,27 +19,8 @@ CORS(app)
 # Load data (deserialize)
 with open('models/encoded.pickle', 'rb') as handle:
     encoded_dict = pickle.load(handle)
-model_path = "models/XgBoost.pickle"
+model_path = "models/XgBoost.joblib"
 model= joblib.load(model_path)
-
-
-@app.route('/api', methods=['POST','GET'])
-def predict():
-    # Get the data from the POST request.
-    json_data = request.get_json(force=True)
-
-    # Convert json data to dataframe
-    df = pd.DataFrame.from_dict(pd.json_normalize(json_data), orient='columns')
-
-    # Pre-process and make prediction using model loaded from disk as per the data.
-    data = preprocess_and_predict(df,encoded_dict)
-
-    # Predict with Model
-    prediction = model.predict(data)
-
-    # Take the first value of prediction
-    output = prediction[0]
-    return jsonify(output)
 
 
 @app.route("/prediction")
@@ -95,6 +74,21 @@ def prediction():
     data = preprocess_and_predict(df,encoded_dict)
     prediction = model.predict(data)
     return str(prediction[0])
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
